ac62c42f51efee511136f97459bd7ae489de5bb6
Steved <hoagsd@gmail.com>
2017-12-09 03:44:04 -0500

    Add base of first class.

---
+++ src/Incarnate.java
@@ -0,0 +1,43 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Incarnate extends Applet implements Runnable, KeyListener{
+
+    public Incarnate() {
+
+    }
+
+    public void init() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+    public void run() {
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
